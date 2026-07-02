69a10afb706fbd0c600743339451b4d984d604b7
abhaykmr28 <abhaykmr.in@gmail.com>
2026-07-02 22:45:52 +0530

    Added Q01 Sum Of Array

---
+++ 1_Array/Q01_sumOfArray.java
@@ -0,0 +1,20 @@
+public class Q01_sumOfArray{
+    public static void main(String[] args) {
+
+        // Q-> sum of array
+        
+        int arr[]= {10,20,30,40,50};
+        int n= arr.length;
+        int sum = 0;
+
+        for( int i=0; i<n; i++){
+            int value = arr[i];
+            sum= sum+value;
+
+        }
+        // sum is ready to print
+
+        System.out.println(sum);
+        }
+    
+}
