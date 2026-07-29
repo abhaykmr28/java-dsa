53da676dce96f39a3ef3318e4b7b4ddb6656f93f
abhaykmr28 <abhaykmr.in@gmail.com>
2026-07-29 23:40:01 +0530

    Solved Binary Search problem

---
+++ 03_Binary_Search/Q2_LowerBound.java
@@ -0,0 +1,34 @@
+public class Q2_LowerBound {
+
+static int GetLowerBound(int arr[],int target){
+    int n=arr.length;
+    int  s=0;
+    int e=n-1;
+    int ans=-1;
+
+    while (s<=e) {
+        int mid=s+(e-s)/2;
+
+        if(arr[mid]>=target){
+            ans=mid;  // ans store
+            e=mid-1;   // move left
+
+        }
+
+        else{
+            // move right
+            s=mid+1;
+        }
+        
+    }
+
+    return ans;
+}
+public static void main(String[] args) {
+    int arr[]={10,20,30,30,30,30,30,40};
+    int target=30;
+    int lowerbound=GetLowerBound(arr, target);
+    System.out.println(lowerbound);
+}
+    
+}
