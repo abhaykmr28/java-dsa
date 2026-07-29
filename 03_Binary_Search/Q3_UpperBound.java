e16fb13aee99c4a29e4cfcb1f413e92dd2383172
abhaykmr28 <abhaykmr.in@gmail.com>
2026-07-29 23:57:46 +0530

    Solved Binary Search problem

---
+++ 03_Binary_Search/Q3_UpperBound.java
@@ -0,0 +1,32 @@
+public class Q3_UpperBound {
+
+    static int GetUpperBound(int arr[],int target){
+        int s=0;
+        int n=arr.length;
+        int e=n-1;
+        int ans=-1;
+        while (s<=e) {
+            int mid=s+(e-s)/2;
+
+            if(arr[mid]<=target){
+                s=mid+1;
+            }
+            else{
+                // store ans
+                ans=mid;
+                // move left
+                e=mid-1;
+
+            }
+            
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+    int arr[]={10,20,30,30,30,30,30,40};
+    int target=30;
+   int upperbound=GetUpperBound(arr, target);
+   System.out.print(upperbound);
+}
+    
+}
