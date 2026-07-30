3e5550ea17f7dc66cd5a513d0e34262bc7a09ae8
abhaykmr28 <abhaykmr.in@gmail.com>
2026-07-30 23:58:22 +0530

    Solved Binary Search problem

--- 03_Binary_Search/Q04_NumberOfOccurence.java
+++ 03_Binary_Search/Q04_NumberOfOccurence.java
@@ -1,4 +1,58 @@
 public class Q04_NumberOfOccurence {
-    
+    static int GetLowerBound(int arr[],int target){
+    int n=arr.length;
+    int  s=0;
+    int e=n-1;
+    int ans=n;
+
+    while (s<=e) {
+        int mid=s+(e-s)/2;
+
+        if(arr[mid]>=target){
+            ans=mid;  // ans store
+            e=mid-1;   // move left
+
+        }
+
+        else{
+            // move right
+            s=mid+1;
+        }
+        
+    }
+
+    return ans;
+}
+
+     static int GetUpperBound(int arr[],int target){
+        int s=0;
+        int n=arr.length;
+        int e=n-1;
+        int ans=n;
+        while (s<=e) {
+            int mid=s+(e-s)/2;
+
+            if(arr[mid]<=target){
+                s=mid+1;
+            }
+            else{
+                // store ans
+                ans=mid;
+                // move left
+                e=mid-1;
+
+            }
+            
+        }
+        return ans;
+    }
+
+    static int Countfreq(int arr[],int target){
+        int lbIndex=GetLowerBound(arr, target);
+        int ubIndex=GetUpperBound(arr, target);
+        int ans=ubIndex-lbIndex;
+        return ans;
+    }
+
     
 }
