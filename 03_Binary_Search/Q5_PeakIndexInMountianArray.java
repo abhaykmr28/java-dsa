f3f3a9e9082be579c5d12e7374b25e6d82da379d
abhaykmr28 <abhaykmr.in@gmail.com>
2026-08-02 19:03:45 +0530

    Solved Binary Search problem

--- 03_Binary_Search/Q5_PeakIndexInMountianArray.java
+++ 03_Binary_Search/Q5_PeakIndexInMountianArray.java
@@ -1,23 +1,25 @@
 public class Q5_PeakIndexInMountianArray {
 
+    
+
     public int peakIndexInMountainArray(int[] arr) {
 
         int s = 0;
         int e = arr.length - 1;
+        int ans = -1;
 
         while (s < e) {
 
             int mid = s + (e - s) / 2;
 
             if (arr[mid] < arr[mid + 1]) {
-                // Peak right side me hai
                 s = mid + 1;
             } else {
-                // Peak mid par ya left side me hai
+                ans = mid;   // optional
                 e = mid;
             }
         }
 
-        return s; // s == e == peak index
+        return s;   // ya return ans;
     }
 }
