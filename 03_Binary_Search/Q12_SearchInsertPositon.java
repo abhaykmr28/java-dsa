1be39874525fe91aef0da19b3f4d4023a4b1dc28
abhaykmr28 <abhaykmr.in@gmail.com>
2026-08-07 23:12:14 +0530

    Solved Binary Search problem

---
+++ 03_Binary_Search/Q12_SearchInsertPositon.java
@@ -0,0 +1,40 @@
+public class Q12_SearchInsertPositon {
+    
+
+    static int searchInsert(int[] nums, int target) {
+
+        int s = 0;
+        int e = nums.length - 1;
+
+        while (s <= e) {
+
+            int mid = s + (e - s) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            else if (nums[mid] < target) {
+                s = mid + 1;
+            }
+
+            else {
+                e = mid - 1;
+            }
+        }
+
+        // Target not found.
+        // 's' is the correct insertion position.
+        return s;
+    }
+
+    public static void main(String[] args) {
+
+        int nums[] = {1, 3, 5, 6};
+
+        System.out.println(searchInsert(nums, 5)); // 2
+        
+    }
+}
+    
+
