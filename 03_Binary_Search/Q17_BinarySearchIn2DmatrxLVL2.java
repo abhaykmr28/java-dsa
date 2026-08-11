acc969b780bcb12c0a3c58c3258e920295a687fa
abhaykmr28 <abhaykmr.in@gmail.com>
2026-08-11 23:15:58 +0530

    Solved Binary Search problem

---
+++ 03_Binary_Search/Q17_BinarySearchIn2DmatrxLVL2.java
@@ -0,0 +1,39 @@
+public class Q17_BinarySearchIn2DmatrxLVL2 {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int totalrow=matrix.length;
+        int totalcol=matrix[0].length;
+        // n-> total element
+
+        int n = totalrow*totalcol;
+        // think like 1d array
+        int s=0;  
+        int e=n-1;
+
+        // binary serch
+        while(s<=e){
+            int mid=s+(e-s)/2;
+
+            int rowindex=mid/totalcol;
+            int colindex=mid%totalcol;
+            
+            if(matrix[rowindex][colindex]==target){
+                return true;
+
+            }
+
+            // mid<target search in right half
+
+            else if(matrix[rowindex][colindex]<target){
+                s=mid+1;
+            }
+
+            else{
+                e=mid-1;
+            }
+        }
+
+        return false;
+        
+    }
+    
+}
