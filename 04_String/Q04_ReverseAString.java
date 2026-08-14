101d108ef8aece6710b9212e147a4cc20e458fe7
abhaykmr28 <abhaykmr.in@gmail.com>
2026-08-15 00:20:09 +0530

    Solved String problem

---
+++ 04_String/Q04_ReverseAString.java
@@ -0,0 +1,30 @@
+public class Q04_ReverseAString {
+    static void reverseString(String str){
+
+        char arr[]=str.toCharArray();
+        int n=arr.length;
+
+        int i=0;
+        int j=n-1;
+         
+        while(i<j){
+            
+            char temp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+            i++;
+            j--;
+        }
+        System.out.println(arr);
+    }
+
+    public static void main(String[] args) {
+        String str="Happy";
+
+        reverseString(str);
+        
+       
+      
+    }
+    
+}
