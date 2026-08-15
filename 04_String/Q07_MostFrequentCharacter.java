6a48d7045c48bdd609b70c9c2c27102d1f10a363
abhaykmr28 <abhaykmr.in@gmail.com>
2026-08-16 00:01:34 +0530

    Solved String problem

--- 04_String/Q07_MostFrequentCharacter.java
+++ 04_String/Q07_MostFrequentCharacter.java
@@ -1,3 +1,23 @@
 public class Q07_MostFrequentCharacter {
-    
+
+    static char getMaxOccurence(String s){
+
+        int[] freq = new int[26];
+
+        for(int i=0; i<s.length(); i++) {
+            freq[s.charAt(i) - 'a']++;
+        }
+
+        int maxfreq = -1;     // ← semicolon missing
+        int ans = 'a';
+
+        for(int i=0; i<26; i++){
+            if(freq[i] > maxfreq){
+                maxfreq = freq[i];
+                ans = (char)(i + 'a');
+            }
+        }
+
+        return (char) ans;   // ← char return karna hai
+    }
 }
