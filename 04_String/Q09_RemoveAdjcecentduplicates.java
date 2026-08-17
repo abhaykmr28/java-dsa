3103cc76ab72dab5f3a48923334b0f2ea1e4cc10
abhaykmr28 <abhaykmr.in@gmail.com>
2026-08-17 22:34:36 +0530

    Solved String problem

---
+++ 04_String/Q09_RemoveAdjcecentduplicates.java
@@ -0,0 +1,32 @@
+public class Q09_RemoveAdjcecentduplicates {
+
+    static String removeadjcentdupplicate(String s) {
+
+    StringBuilder ans = new StringBuilder();
+    int n = s.length();
+
+    for (int i = 0; i < n; i++) {
+
+        char currentch = s.charAt(i);
+
+        if (ans.length() > 0 &&
+            ans.charAt(ans.length() - 1) == currentch) {
+
+            ans.deleteCharAt(ans.length() - 1);
+
+        } else {
+            ans.append(currentch);
+        }
+    }
+
+    return ans.toString();
+}
+    public static void main(String[] args) {
+        String s="abbaac";
+
+       String finalans= removeadjcentdupplicate(s);
+
+       System.out.println(finalans);
+    }
+    
+}
